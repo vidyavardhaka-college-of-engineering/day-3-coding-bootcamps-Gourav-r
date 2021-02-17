5cb54a386720c7d2bd16dc3188b7ce6bc825d640
Gourav R <20cse0081@vvce.ac.in>
2021-02-17 11:54:37 +0000

    To build a half pyramid

--- Problem_4.c
+++ Problem_4.c
@@ -5,12 +5,18 @@
 
 int main()
 {
-  	int n;
+  	int a, b, n;
 
   	scanf("%d", &n);
 
   	//Write your code here
-  	
+  	for(a=0;a<=n;a++)
+    {
+      for(b=1;b<=a;b++){
+        printf("*");
+      }
+      printf("\n");
+    }
 
   	return 0;
 }
